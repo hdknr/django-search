ef86dcefbd822be3ec971cec4506a3a8372e67b4
hdknr <gmail@hdknr.com>
2016-09-18 12:10:50 +0000

    default for Elasticsearch

---
+++ lib/djsearch/settings.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from django.conf import settings
+
+DJSEARCH = getattr(settings, 'DJSEARCH', {})
+
+ELASTICSEARCH = {
+    'hosts': ['localhost'],
+    'port': 9200,
+    'index': 'django',
+}
+ELASTICSEARCH.update(DJSEARCH.get('elasticsearch', {}))
